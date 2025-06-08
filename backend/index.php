b08ce645a80dc8ea85f0e66bb17a649bdcb3886c
anja-sehovac <anja.sehovac@stu.ibu.edu.ba>
2025-06-08 14:48:44 +0200

    CORS

--- backend/index.php
+++ backend/index.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://cuppatea-frontend-gjxs4.ondigitalocean.app");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
