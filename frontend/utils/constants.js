1d2c42437fdb50cf3e97f99b6f2b7bd591e72635
anja-sehovac <anja.sehovac@stu.ibu.edu.ba>
2025-06-08 16:42:49 +0200

    /

--- frontend/utils/constants.js
+++ frontend/utils/constants.js
@@ -1,9 +1,9 @@
 var Constants = {
     get_api_base_url: function () {
         if (location.hostname === "localhost" ) {
-            return "http://localhost/web_project/backend/";
+            return "http://localhost/web_project/backend";
         } else {
-            return "https://cuppatea-vv3qj.ondigitalocean.app/";
+            return "https://cuppatea-vv3qj.ondigitalocean.app";
         }
     },
     USER_ROLE: "user",
